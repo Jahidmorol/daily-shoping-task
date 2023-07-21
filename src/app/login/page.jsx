e357e3b9ca0369b6031570f826f9e4f44ef26415
jahid <jahidmorol2@gmail.com>
2023-07-21 23:20:50 +0600

    login page setup successfull

---
+++ src/app/login/page.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="flex items-center justify-center w-[80%] mx-auto h-screen">
+      <div className="shadow rounded-lg bg-white">
+        <img
+          src="/login.png"
+          alt="loginPhoto"
+          className="w-[18rem] rounded-lg"
+        />
+      </div>
+
+      <div className="w-[38%]">
+        <div className=" sm:mx-auto sm:w-full sm:max-w-md">
+          <div className=" py-8 px-4  sm:px-10">
+            <div className="mb-3">
+              <h2 className="font-bold text-lg">Login</h2>
+              <p className="text-sm">Donec tortor quam at duis tortor.</p>
+            </div>
+            <form>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <input
+                    id="email"
+                    name="email"
+                    placeholder="user@example.com"
+                    type="email"
+                    required
+                    className=" w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                  <div className="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                    <svg
+                      className="h-5 w-5 text-red-500"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-3">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    type="password"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-2 flex items-center justify-end">
+                <div className="text-sm leading-5">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+
+              <div className="mt-3">
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border  text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-transparent hover:text-black  hover:border-black focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </button>
+                </span>
+              </div>
+              <div class="my-3 border-b text-center">
+                <div class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                  Or
+                </div>
+              </div>
+              <div className="mt-3">
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-black text-sm font-medium rounded-md text-black bg-transparent hover:bg-blue-500 hover:text-white hover:border-transparent focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Register Now
+                  </button>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
